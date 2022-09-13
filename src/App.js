af02c8c8229f2b5178456a8476bea3ab64b2ba63
Jake Balsamo <balsamoj@wit.edu>
2022-09-12 22:24:17 -0400

    Downgrade React to 17.0.2; install react-pixi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Stage from "@inlet/react-pixi"
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Stage>
+      
+      </Stage>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +24,14 @@ function App() {
       </header>
     </div>
   );
+} */
+
+function App () {
+  return (
+    <Stage>
+
+    </Stage>
+  )
 }
 
 export default App;
